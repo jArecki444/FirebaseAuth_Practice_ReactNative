702841b8860c8c0bd3008ea1119bd2a8034a6495
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2019-06-13 12:09:13 +0200

    Added a few reusable components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Header } from './components/common';
 
 class App extends Component {
   render() {
     return (
-      <View >
-        <Text >Welcome to React Native!</Text>
+      <View>
+        <Header headerText="Authentication" />
+        <Text>Welcome to React Native!</Text>
       </View>
     );
   }
