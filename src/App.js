a1f325332d91775a602496ad3b8d59c6f70a7168
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2019-06-13 13:09:36 +0200

    Initialize firebase connection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBEv_x03CYsH-8HJlzP4q-_CXXt8lpY_HE',
+      authDomain: 'authdsafgdsfgfsdg.firebaseapp.com',
+      databaseURL: 'https://authdsafgdsfgfsdg.firebaseio.com',
+      projectId: 'authdsafgdsfgfsdg',
+      storageBucket: 'authdsafgdsfgfsdg.appspot.com',
+      messagingSenderId: '891424480028',
+      appId: '1:891424480028:web:195f9ec5d1108fcd'
+    });
+  }
   render() {
     return (
       <View>
